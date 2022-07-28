3fb94c028117e44fc02dc26b02b0ba094a20cba7
Ilya Radchenko <knownasilya@gmail.com>
2022-07-28 10:23:34 -0400

    chore: remove comments

--- apps/test-chess/app/routes/application/controller.ts
+++ apps/test-chess/app/routes/application/controller.ts
@@ -1,4 +1,4 @@
-import { Chess, ChessInstance, PieceType, Square } from 'chess.js';
+import { Chess, PieceType, Square } from 'chess.js';
 
 import Controller from '@ember/controller';
 
@@ -6,7 +6,6 @@ import Chessboard from 'ember-chessboard/components/chessboard';
 
 export default class ApplicationController extends Controller {
   Chessboard = Chessboard;
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
   chess = new Chess();
 
   get board(): ({
@@ -14,7 +13,6 @@ export default class ApplicationController extends Controller {
     color: 'b' | 'w';
     square: Square;
   } | null)[][] {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
     return this.chess.board();
   }
 }
